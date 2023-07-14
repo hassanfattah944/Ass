092990a5b92a0b780490ecb6acf883c26ffe27e0
nftrtmjh54 <hassanfattah944@gmail.com>
2023-07-14 23:05:58 +0300

    first commit

---
+++ Assignment-02.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def count_digits(n):
+    if n //10 == 0:
+        return 1
+    else:
+        return 1+count_digits(n//10)
+    
+def find_max(lst):
+    if len(lst) == 0:
+        return 0
+    elif len(lst) == 1:
+        return lst[0]
+    else:
+        max_rest = find_max(lst[1:])
+        return lst[0] if lst[0] > max_rest else max_rest
+    
+def count_tags(html, tag):
+    start_tag = "<" + tag + ">"
+    end_tag = "</" + tag + ">"
+    start_index = html.find(start_tag)
+    end_index = html.find(end_tag)
+    if start_index == -1 or end_index == -1:
+        return 0
+    else:
+        return 1 + count_tags(html[end_index + len(end_tag):], tag)
+    
+        
+while True:
+    choice=input("Please Enter a choice:")
+    if choice == "1":
+        n1=int(input("Enter an integer:"))
+        result=count_digits(n1)
+        print("The number of Digits",n1,"is",result)
+    elif choice == "2":
+        List_one = list(map(int, input("Enter a list of integers: ").split()))
+        max_value = find_max(List_one)
+        print("Maximum value:", max_value)
+        
+    elif choice == "3":
+        html = """
+        <html>
+        <head>
+        <title>My Website</title>
+        </head>
+        <body>
+        <h1>Welcome to my website!</h1>
+        <p>Here you'll find information about me and my hobbies.</p>
+        <h2>Hobbies</h2>
+        <ul>
+        <li>Playing guitar</li>
+        <li>Reading books</li>
+        <li>Traveling</li>
+        <li>Writing cool h1 tags</li>
+        </ul>
+        </body>
+        </html>
+        """
+        tag = input("Enter a tag: ")
+        count = count_tags(html, tag)
+        print("Occurrences of the tag:", count)    
+
+    elif choice == "4":
+        break
+    else:
+        print("Invalid choice. Please try again.\n")  
+
+
+# In[ ]:
+
+
+
+
